c570a9195f25ade9279f1354ad1e6ebcb26a7c74
XingyuGuUCSD <xingyuguseu@gmail.com>
2018-08-05 20:05:50 -0700

    finish vuex part

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,58 +1,29 @@
 <template>
-    <div class="posts-page">
-        <PostList :posts="loadPosts"/>
-    </div>    
+  <div class="posts-page">
+    <PostList :posts="loadedPosts" />
+  </div>
 </template>
 
 <script>
-import PostList from '@/components/Posts/PostList.vue'
-export default{
-    components:{
-        PostList,
-    },
-    asyncData(context, callback){
-        //verify that asyncData is running on the server side
-        console.log(context);
-        console.log("asyncData executed...");
-        setTimeout(() => {
-        callback(null, {loadPosts: [
-            {
-            id:'1',
-            author: 'Alee GU',
-            title: '0My blog',
-            previewText: 'balabala',
-            thumbnaillink: 'https://res.cloudinary.com/cmgverticals/image/upload/c_crop,g_north_west,h_2377,w_4509,x_0,y_200/c_fill,h_628,q_80,w_1200/e_sharpen,f_auto,fl_lossy,q_auto/v1523999146/GettyImages-916732976_s6udvr.jpg'
-            },
-            {
-            id:'2',
-            author: 'Alee GU',
-            title: '1My blog',
-            previewText: 'balabala',
-            thumbnaillink: 'https://res.cloudinary.com/cmgverticals/image/upload/c_crop,g_north_west,h_2377,w_4509,x_0,y_200/c_fill,h_628,q_80,w_1200/e_sharpen,f_auto,fl_lossy,q_auto/v1523999146/GettyImages-916732976_s6udvr.jpg'
-            },
-            {
-            id:'3',
-            author: 'Alee GU',
-            title: '2My blog',
-            previewText: 'balabala',
-            thumbnaillink: 'https://res.cloudinary.com/cmgverticals/image/upload/c_crop,g_north_west,h_2377,w_4509,x_0,y_200/c_fill,h_628,q_80,w_1200/e_sharpen,f_auto,fl_lossy,q_auto/v1523999146/GettyImages-916732976_s6udvr.jpg'
-            }
-            ] 
-        })
-        },1500);
-    },
-    created(){
-        this.$store.dispatch('setPosts', this.loadPosts);
+import PostList from "@/components/Posts/PostList";
+
+export default {
+  components: {
+    PostList
+  },
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts
     }
-}
+  }
+};
 </script>
 
+
 <style scoped>
-.posts-page{
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
-
-
--- store/index.js
+++ store/index.js
@@ -1,25 +1,47 @@
-import Vuex from 'vuex'
+import Vuex from "vuex";
+
 const createStore = () => {
-    return new Vuex.Store({
-        state: {
-            loadedPosts:[]
-        },
-        mutations: {
-            setPosts(state, posts){
-                state.loadedPosts = posts;
-            }
-        },
-        actions: {
-            setPosts(vuexContext, posts){
-                vuexContext.commit('setPosts', posts);
-            }
-        },
-        getters:{
-            loadedPosts(state){
-                return state.loadedPosts;
-            }
-        }
-    });
+  return new Vuex.Store({
+    state: {
+      loadedPosts: []
+    },
+    mutations: {
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
+      }
+    },
+    actions: {
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit("setPosts", [
+              {
+                id: "1",
+                title: "First Post",
+                previewText: "This is our first post!",
+                thumbnaillink: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              },
+              {
+                id: "2",
+                title: "Second Post",
+                previewText: "This is our second post!",
+                thumbnaillink: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              }
+            ]);
+            resolve();
+          }, 1000);
+        });
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
+      }
+    },
+    getters: {
+      loadedPosts(state) {
+        return state.loadedPosts;
+      }
+    }
+  });
 };
 
-export default createStore
+export default createStore;
